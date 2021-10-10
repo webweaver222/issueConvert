ce0c6a8c378029167b706ecb1b1bac8d3ee759d9
Alex Teisheira <fukirx@gmail.com>
2021-10-10 08:32:16 +0300

    deploy config

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+import { Router } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
 import App from "./components/app";
 import sprite from "../src/resources/svg/sprite.svg";
 
+const history = createBrowserHistory({ basename: "issues" });
+
 ReactDom.render(
-  <>
+  <Router history={history}>
     <div
       dangerouslySetInnerHTML={{ __html: sprite }}
       style={{ display: "none" }}
     ></div>
     <App />
-  </>,
+  </Router>,
   document.getElementById("root")
 );
